cafbc531bb3d41351e32c1144a9d24d3cf12c641
condorfromthewest <64895385+condorfromthewest@users.noreply.github.com>
2020-06-09 11:22:18 +0200

    Update pass-checker.py

--- pass-checker.py
+++ pass-checker.py
@@ -3,33 +3,39 @@ import hashlib
 
 #the API uses k-anonimity method, thus only requires the first 5 characters of the hashed password
 def req_api_data(query_char):
+    ''' Connects with the website API and runs a request for specific character API information.
+    '''
     url = 'https://api.pwnedpasswords.com/range/' + query_char
     res = requests.get(url)
     if res.status_code != 200:
         raise RuntimeError(f'Error fetching {res.status_code}. Check API and try again.')
     return res
 
-#Aquí creo mi hash password a partir de mi input 'password' y luego llamo a mi función get_leak_counts
-#para ver si mi password ya se encuentra en el sistema del API y ver cuántas veces fue vulnerada
 def pwned_api_check(password):
+    '''Creates a hash from the user inputed password and then calls get_leak_counts function
+    to check password for possible vulnerabilities.
+    '''
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
     first5_char, tail_char = sha1password[:5], sha1password[5:]
     response = req_api_data(first5_char)
-    #print(response.text) me muestra todos los endings coincidentes con mis first5_char, y cuantas veces aparece
     return get_pass_leak_counts(response, tail_char)
     
-#Esta función me devuelve todos los tail_char de hash passwords que coinciden con mis primeros
-#first5_char. voy a ver una impresión de listas con todos los tail_char en [0] y la cuenta de cuántas veces
-#aparecen en [1]
 def get_pass_leak_counts(hashes, hash_to_check):
+    '''Runs a query comparing the first 5 characters from the hash with all possible tail characters that 
+    match said first 5 characters. Returns a printed list with all tail characters from the hash followed by a 
+    count of how many times they appear in the pwned database.
+    '''
     hashes = (line.split(':') for line in hashes.text.splitlines())
     for h, count in hashes:
-    #con este if evaluo a mi tail_char para ver si aparece en la lista
+    #check my tail_char for appearance in the full tail character list
         if h == hash_to_check:
             return count
     return 0
 
 def main(args):
+    ''' Runs the API request and query from user-inputed passwords. Returns a print of the password followed by 
+    the count, if it appears in the database, or a confirmation that it has not been pwned.
+    '''
     for password in args:
         count = pwned_api_check(password)
         if count:
